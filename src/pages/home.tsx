b408b4b133ba48c05fde69ef13a6e68d3a7d4d33
azmye <mhmmdazmye@gmail.com>
2024-07-24 22:07:33 +0700

    chore: check env on vercel

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { Articles, SearchBar } from "../sections/home";
 
 export const Home = () => {
+  console.log(process.env.API_KEY);
   return (
     <Fragment>
       <SearchBar />

5fc81a3229426094a3c40dc2176b9d86bbbe3398
azmye <mhmmdazmye@gmail.com>
2024-07-24 22:32:22 +0700

    fix: try to avoid cors error

--- src/redux/api/root/api-root.ts
+++ src/redux/api/root/api-root.ts
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseUrl = import.meta.env.VITE_APP_API_URL;
+
 export const apiRoot = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api", // Use proxy path
+    baseUrl: baseUrl, // Use proxy path
     prepareHeaders: (headers) => {
       headers.set("X-Api-Key", `${import.meta.env.VITE_APP_API_KEY}`);
       return headers;

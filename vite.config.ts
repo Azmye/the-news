ec533ff7a4a0198ac57543f5838a9f8bdb561e0d
azmye <mhmmdazmye@gmail.com>
2024-07-24 22:32:43 +0700

    fix: try to avoid cors error

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": process.env,
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://newsapi.org/v2",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
 });

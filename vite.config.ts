fd8f188dc81f439785d1caa48bbadb46a6dfea1a
azmye <mhmmdazmye@gmail.com>
2024-07-24 22:23:03 +0700

    fix: api error won't fetch

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   define: {
     "process.env": process.env,
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://newsapi.org/v2", // Target API server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Rewrite path
+      },
+    },
+  },
 });
